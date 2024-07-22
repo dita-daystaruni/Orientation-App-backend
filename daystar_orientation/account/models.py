6a85e30946a5aa3f0b17181eefb83c4f69bc3c0b
Clive Sasaka <clivesasaka@gmail.com>
2024-07-22 08:56:53 +0300

    Made the email, phone ad course optional fields as per the user design

--- daystar_orientation/account/models.py
+++ daystar_orientation/account/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Account(AbstractUser):
     name = models.CharField(max_length=100)
     admission_number = models.CharField(max_length=20, unique=True)
-    course = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15)
-    email = models.EmailField(unique=True)
+    course = models.CharField(max_length=100, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
 
 
     groups = models.ManyToManyField(
