e8407fb5fb6284e837d04c6b8dd1a3833331b8d2
Baraka Mnjala Mbugua <148796574+eiidoubleyuwes@users.noreply.github.com>
2024-08-05 20:49:35 +0300

    Feat:Update settings.py
    Added DB folder

--- daystar_orientation/daystar_orientation/settings.py
+++ daystar_orientation/daystar_orientation/settings.py
@@ -110,7 +110,7 @@ DJOSER = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
     }
 }
 
