791614c7a1aad45eea47701009d0b69b3cad44d4
Clive Sasaka <clivesasaka@gmail.com>
2024-08-23 18:50:30 +0300

    Mod: Added file validators and size verification to only 30MB

--- daystar_orientation/account/serializers.py
+++ daystar_orientation/account/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from .models import Account, Documents
-
+import mimetypes
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ['id', 'first_name', 'last_name', 'username', 'email', 'admission_number', 'course', 'phone_number', 'user_type', 'campus', 'gender', 'accomodation', 'checked_in', 'parent', 'password']
+        fields = [
+            'id', 'first_name', 'last_name', 'username', 'email', 'admission_number', 
+            'course', 'phone_number', 'user_type', 'campus', 'gender', 
+            'accomodation', 'checked_in', 'parent', 'password'
+        ]
         read_only_fields = ['password']
 
     def create(self, validated_data):
@@ -31,6 +35,26 @@ class PasswordChangeSerializer(serializers.Serializer):
         return value
 
 class DocumentSerializer(serializers.ModelSerializer):
+
+    def validate_file(self, value):
+        mime_type, encoding = mimetypes.guess_type(value.name)
+
+        valid_mime_types = [
+            'application/pdf', 
+            'image/jpeg',       
+            'image/png',        
+            'image/gif',        
+            'image/bmp',     
+            'text/plain',       
+            'application/msword',  
+            'application/vnd.openxmlformats-officedocument.wordprocessingml.document' 
+        ]
+        
+        if mime_type not in valid_mime_types:
+            raise serializers.ValidationError('Unsupported file type.')
+        
+        return value
+
     class Meta:
         model = Documents
         fields = ['id', 'user', 'title', 'file', 'uploaded_at', 'description']
