430c875afa25cbbcff78bba3752723d17da5dff8
Clive Sasaka <clivesasaka@gmail.com>
2024-08-27 13:40:37 +0300

    Mod: Removed the image and video fields from the events app as requested. Reduced upload size to 15mb alone

--- daystar_orientation/events/models.py
+++ daystar_orientation/events/models.py
@@ -4,12 +4,10 @@ class Event(models.Model):
     '''Model for the events'''
     title = models.CharField(max_length=200)
     description = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
     venue = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='Photos_Videos/', null=True, blank=True)
-    video = models.FileField(upload_to='Photos_Videos/', null=True, blank=True)
 
     def __str__(self):
         return self.title
