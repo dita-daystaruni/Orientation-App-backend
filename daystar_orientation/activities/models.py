448f01bd078348fc45736462eb84043904efb476
Clive Sasaka <clivesasaka@gmail.com>
2024-08-28 23:01:27 +0300

    Mod: Added a date field for activities

--- daystar_orientation/activities/models.py
+++ daystar_orientation/activities/models.py
@@ -4,6 +4,7 @@ class Activity(models.Model):
     '''Model for the activities'''
     title = models.CharField(max_length=200)
     description = models.TextField()
+    date = models.DateField(null=True, blank=True)
     location = models.CharField(max_length=200, null=True, blank=True)
     start_time = models.TimeField(null=True, blank=True)
     end_time = models.TimeField(null=True, blank=True)
