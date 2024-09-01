b9fcf5b463ff06b914c720a78fdb43e9ee8fb620
Jess <jessgatura@gmail.com>
2024-09-02 02:19:36 +0300

    feat: adding devices and sending notifications

--- daystar_orientation/notifications/urls.py
+++ daystar_orientation/notifications/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import NotificationList, NotificationDetail, RecentNotificationList
+from .views import NotificationList, NotificationDetail, RecentNotificationList, RegisterDevice
 
 urlpatterns = [
     path('notifications/', NotificationList.as_view(), name='notification-list'),
     path('notifications/<int:pk>/', NotificationDetail.as_view(), name='notification-detail'),
     path('recent/', RecentNotificationList.as_view(), name='recent-notifications'),
+    path('register-device/', RegisterDevice.as_view(), name='register_device'),
 ]
