924c545c6246c04944cec9a8c592c5c88fb424aa
Clive Sasaka <clivesasaka@gmail.com>
2024-09-08 10:28:34 +0300

    Mod: Fixed the static url issue and added the trailing slash on settings

--- daystar_orientation/daystar_orientation/urls.py
+++ daystar_orientation/daystar_orientation/urls.py
@@ -24,3 +24,5 @@ urlpatterns = [
     path("<path:resource>", flutter_redirect),
     path('hods/', include('hods.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
